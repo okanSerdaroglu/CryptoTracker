f8821e5c9e24d19c453d53946fe292fc70ef2e21
okanSerdaroglu <ibrahim.serdaroglu@atmosware.turkcell.com.tr>
2024-10-27 12:01:53 +0300

    coin and coinList ui

--- CryptoTracker-initial/app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
+++ CryptoTracker-initial/app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
@@ -13,6 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 
+/**
+ * generally package with feature by feature can be the best choose.
+ * each feature should have three different packages as data, domain and presentation.
+ * presentation -> UI related stuff
+ * domain -> all business logics should be there, the most important think is in domain layer
+ *           there should not be any android specific dependency like, context or activity.
+ * data -> all data related stuff should be there. Network models
+ */
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
