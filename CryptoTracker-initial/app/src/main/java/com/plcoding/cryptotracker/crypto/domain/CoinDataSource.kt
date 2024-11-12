5e92fe450eb7c61cd7ce3484bdbdb870c79707a1
okanSerdaroglu <okanserdaroglu2@gmail.com>
2024-11-12 23:06:47 +0300

    loading coin details

--- CryptoTracker-initial/app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
+++ CryptoTracker-initial/app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
@@ -2,6 +2,8 @@ package com.plcoding.cryptotracker.crypto.domain
 
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
+import com.plcoding.cryptotracker.crypto.data.networking.dto.CoinHistoryDto
+import java.time.ZonedDateTime
 
 /**
  * this is definition of CoinDataSource, the implementation should be in data layer because domain does not know about data layer
@@ -10,5 +12,11 @@ import com.plcoding.cryptotracker.core.domain.util.Result
 interface CoinDataSource {
 
     suspend fun getCoins(): Result<List<Coin>, NetworkError>
+
+    suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError>
 
 }
